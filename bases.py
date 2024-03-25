33f80a3215bacf6a9a2a8b4da0c3f30232afa336
Paulo <paulomarcellor@gmail.com>
2024-03-25 10:12:29 -0300

    bases

--- bases.py
+++ bases.py
@@ -1,22 +1,74 @@
 import sqlite3 as sql
 
+createProductsTable = """
+
+    CREATE TABLE PRODUCTS (
+        product_id INTEGER PRIMARY KEY,
+        name VARCHAR(200),
+        category VARCHAR(100)
+    );
+
+"""
+
+createClientsTable = """
+
+    CREATE TABLE CLIENTS (
+        client_id INTEGER PRIMARY KEY,
+        name VARCHAR(200),
+        age INTEGER,
+        sex CHAR,
+        marital_status VARCHAR(20),
+        email VARCHAR(100),
+        phone VARCHAR(20),
+        occupation VARCHAR(100),
+        preferencial_communication VARCHAR(100),
+        location VARCHAR(100),
+        average_ticket DECIMAL(10, 2)      
+    );
+
+"""
+
+
 createStockTable = """
+
+    CREATE TABLE STOCK (
+        product_id INTEGER,
+        date_in DATE,
+        amount INTEGER,
+        FOREIGN KEY (product_id) REFERENCES PRODUCTS(product_id)
+    );
 
 
 """
 
 createSalesTable = """
 
+    CREATE TABLE Sales (
+        transaction_id INTEGER PRIMARY KEY,
+        sale_date DATE,
+        client_id INTEGER,
+        product_id INTEGER,
+        sold_products VARCHAR(255),
+        quantity_sold INTEGER,
+        unit_price DECIMAL(10, 2),
+        total_price DECIMAL(10, 2),
+        discounts DECIMAL(10, 2),
+        additional_fees DECIMAL(10, 2),
+        payment_method VARCHAR(50),
+        transaction_status VARCHAR(50),
+        delivery_method VARCHAR(50),
+        employee_id INTEGER,
+        sale_location VARCHAR(100),
+        coupon_code_used VARCHAR(50),
+        additional_notes TEXT,
+        FOREIGN KEY (client_id, product_id) REFERENCES PRODUCTS(client_id, product_id)
+        );
 
 """
 
-createProductsTable = """
+conn = sql.connect('commerce.db')
 
-
-"""
-
-createClientsTable = """
-
-
-"""
-
+conn.execute(createProductsTable)
+conn.execute(createClientsTable)
+conn.execute(createStockTable)
+conn.execute(createSalesTable)
