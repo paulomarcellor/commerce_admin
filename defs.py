1ef3c0f7ea1955e9fd57d74fed75bf4a1b0d962d
Paulo <paulomarcellor@gmail.com>
2024-03-27 10:00:13 -0300

    client registration created

--- defs.py
+++ defs.py
@@ -1,36 +1,36 @@
 import sqlite3 as sql
 
-def connectDB(db='commerce.db'):
-    try:
-        conn = sql.connect(db)
-        cursor = conn.cursor()
-        return conn, cursor
-    except sql.Error as e:
-        print(f"It was not possible to connect to database: {e}")
-        return None, None
-
-def closeDB(conn):
-    if conn:
-        try:
-            conn.commit()
-        except sql.Error as e:
-            print(f"Commit error: {e}")
-        finally:
-            try:
-                conn.close()
-            except sql.Error as e:
-                print(f": {e}")
-
 def insertProductDB(product_name, product_category):
     insertProductQuery = """ INSERT INTO PRODUCTS (product_id, name, category) VALUES (?, ?, ?)"""
-    
-    conn = sql.connect(db)
+    conn = sql.connect('commerce.db')
     cursor = conn.cursor()
-
-    product_id = int(cursor.execute(""" SELECT MAX(product_id) FROM PRODUCTS """).fetchone()[0])+1
-
-    cursor.execute(insertProductQuery,(product_id, product_category, product_name))
-
+    if cursor.execute(""" SELECT MAX(product_id) FROM PRODUCTS """).fetchall()[0][0] == None:
+        product_id = 0
+    else:
+        print(cursor.execute(""" SELECT MAX(product_id) FROM PRODUCTS """).fetchall()[0][0])
+        product_id = int(cursor.execute(""" SELECT MAX(product_id) FROM PRODUCTS """).fetchall()[0][0])+1
+    cursor.execute(insertProductQuery,(product_id,product_name,product_category))
     conn.commit()
     conn.close()
-    return 'Product inserted'
+    return 'Registered product'
+
+def insertClientDB(client_name, client_age, client_sex, client_marital_status,\
+                   client_email, client_phone, client_occupation, client_preferencial_communication,\
+                    client_location):
+    insertClientQuery = """ INSERT INTO CLIENTS (client_id, name, age, sex, marital_status,\
+                   email, phone, occupation, preferencial_communication,\
+                    location, average_ticket) VALUES (?,?,?,?,?,?,?,?,?,?,?)"""
+    conn = sql.connect('commerce.db')
+    cursor = conn.cursor()
+    if cursor.execute(""" SELECT MAX(client_id) FROM CLIENTS """).fetchall()[0][0] == None:
+        client_id = 0
+    else:
+        print(cursor.execute(""" SELECT MAX(product_id) FROM CLIENTS """).fetchall()[0][0])
+        client_id = int(cursor.execute(""" SELECT MAX(product_id) FROM CLIENTS """).fetchall()[0][0])+1
+    average_ticket = 0
+    cursor.execute(insertClientQuery,(client_id, client_name, client_age, client_sex, client_marital_status,\
+                   client_email, client_phone, client_occupation, client_preferencial_communication,\
+                    client_location, average_ticket))
+    conn.commit()
+    conn.close()
+    return 'Registered client'
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import sqlite3 as sql
 import pandas as pd
-
 import defs
 
 st.set_page_config(page_title="Commerce Admin", layout="wide", initial_sidebar_state="auto")
@@ -18,22 +17,48 @@ tab1, tab2, tab3 = st.tabs(['Dashboard','Raw Data','Management'])
 
 # MANAGEMENT
 with tab3:
+    st.header('Management System')
     col31, col32, col33 = st.columns(3)
     with col31:
-        insertSale = st.button('Insert Sale', key='btnSale')
-    with col32:
-        insertClient = st.button('Insert Client', key='btnClient')
-    with col33:
-        insertProduct = st.button('Insert Product', key='btnProduct')
-    if insertProduct:
-        with st.form("my_form"):
+        # Sales Form
+        with st.form("Register Sale", clear_on_submit=True):
+            st.header('Register Sale')
             name = st.text_input("Name")
             category = st.text_input("Category")
-            submit_button = st.form_submit_button("Send")
-            if submit_button:
-                st.success('Success!')
-                defs.insertProductDB(name, category)
-                st.success('Success!')
+            sendSale = st.form_submit_button("Register")
+        if sendSale:
+            #defs.insertProductDB(product_name=name, product_category=category)
+            st.success('Success!')
+    with col32:
+        # Clients Form
+        with st.form("Register Client", clear_on_submit=False):
+            st.header('Register Client')
+            client_name=st.text_input('Name')
+            client_age=st.number_input('Age')
+            client_sex=st.selectbox('Sex', ['Male','Female'])
+            client_marital_status=st.selectbox('Marital Status', ['Single','Married', 'Divorced', 'Widowed'])
+            client_email=st.text_input('E-mail')
+            client_phone=st.text_input('Phone Number')
+            client_occupation=st.text_input('Occupation')
+            client_preferencial_communication=st.selectbox('Preferencial Communication', ['Phone', 'SMS', 'E-mail'])
+            client_location=st.text_input('Location')
+            sendClient = st.form_submit_button('Register')
+        if sendClient:
+            defs.insertClientDB(client_name, client_age, client_sex, client_marital_status,\
+                   client_email, client_phone, client_occupation, client_preferencial_communication,\
+                    client_location)
+            st.success(str(client_name) + ' Registered Successfully')
+        
+    with col33:
+        # Products Form
+        with st.form("Register Product", clear_on_submit=True):
+            st.header('Register Product')
+            name = st.text_input("Name")
+            category = st.text_input("Category")
+            sendProduct = st.form_submit_button("Register")
+        if sendProduct:
+            defs.insertProductDB(product_name=name, product_category=category)
+            st.success('Success!')
 
             
     
